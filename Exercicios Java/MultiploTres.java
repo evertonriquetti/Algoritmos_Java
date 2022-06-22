826623142fe1d7c5dbabace810cb6e64d3be8b63
Everton Riquetti <51250841+evertonriquetti@users.noreply.github.com>
2022-06-22 20:44:36 -0300

    Add files via upload
    Add files

---
+++ Exercicios Java/MultiploTres.java
@@ -0,0 +1,11 @@
+public class MultiploTres {
+    public static void main(String args[]) {
+        int i;
+      for (i=0; i<=100; i++){
+        if(i%3==0){
+        System.out.println(i);
+        }
+      }
+    }
+    
+}
