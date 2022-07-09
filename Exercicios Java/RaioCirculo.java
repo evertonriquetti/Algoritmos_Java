275f74ec48cb0e48c3a39c2095bff8ee643ec7dc
Everton Riquetti <51250841+evertonriquetti@users.noreply.github.com>
2022-07-09 18:19:49 -0300

    Add files via upload
    add exercicios

---
+++ Exercicios Java/RaioCirculo.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class RaioCirculo {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		double area, raio, pi = 3.14159;
+		raio = sc.nextDouble();
+		area = pi*raio*raio;
+		System.out.printf("A=%.4f%n", area);
+		sc.close();
+	}
+
+}
